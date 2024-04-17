c0a8cbb55f2fd5dafa2a007041514391035a3f66
Jotham Emmanuel Cheeran <jothamemmanuelcheeran@gmail.com>
2024-04-17 23:43:10 +0530

    second

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 
 
 class MyHomePage extends StatefulWidget {
@@ -15,6 +16,20 @@ class _MyHomePageState extends State<MyHomePage> {
   var destinationLanguage = 'To';
   var outPut = '';
   TextEditingController langController = TextEditingController();
+
+  void Translate(String src, String dest, String input)async{
+    GoogleTranslator translator = new GoogleTranslator();
+    var translation = await translator.translate(input,from: src,to: dest);
+    setState(() {
+      outPut = translation.text.toString();
+    });
+
+    if (src == '--') {
+      setState(() {
+        outPut = 'Failed to translate';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -137,9 +152,93 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                     dropdownColor: Colors.white,
                     icon: Icon(Icons.keyboard_arrow_down),
-                    items: lang.map(String dropDow),
-                  )
+                    items: lang.map((String dropDownStringItem) {
+                      return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                    }).toList(),
+                    onChanged: (String? value){
+                      setState(() {
+                        originLanguage = value!;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  Icon(
+                    Icons.arrow_right_alt_outlined,
+                    color: Colors.white,
+                    size: 40,
+                  ),
+                  SizedBox(
+                    width: 40,
+                  ),
+                  DropdownButton(
+                    focusColor: Colors.white,
+                    iconDisabledColor: Colors.white,
+                    iconEnabledColor: Colors.white,
+                    hint: Text(
+                      destinationLanguage,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    dropdownColor: Colors.white,
+                    icon: Icon(Icons.keyboard_arrow_down),
+                    items: lang.map((String dropDownStringItem) {
+                      return DropdownMenuItem(child: Text(dropDownStringItem),value: dropDownStringItem,);
+                    }).toList(),
+                    onChanged: (String? value){
+                      setState(() {
+                        destinationLanguage = value!;
+                      });
+                    },
+                  ),
                 ],
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: TextFormField(
+                  cursorColor: Colors.white,
+                  autofocus: false,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: InputDecoration(
+                    labelText: 'Please enter the Text',
+                    labelStyle: TextStyle(fontSize: 15,color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 1,
+                      ),
+                    ),
+                      enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white, width: 1),
+                    ),
+                    errorStyle: TextStyle(
+                      color: Colors.red,
+                      fontSize: 15
+                    )
+                  ),
+                  controller: langController,
+                  validator: (value) {
+                    if (value == null || value.isNotEmpty) {
+                      return 'Please re-enter the text again';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Translate',),
+                  style: ElevatedButton.styleFrom(primary: Color(0xff2b3c5a)),
+                ),
               )
             ],
           ),
